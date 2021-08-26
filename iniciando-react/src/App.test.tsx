79652ee46327aa052088d7e74f1d4b747cece8f3
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-26 01:16:56 -0400

    Create React-app

--- iniciando-react/src/App.test.tsx
+++ iniciando-react/src/App.test.tsx
@@ -1,11 +1,9 @@
-import { render } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-describe('Test App', () => {
-  it('Deve renderizar sem erros', () => {
-    const { getByText } = render(<App />);
-
-    expect.assertions(1);
-    expect(getByText('Teste')).toBeTruthy();
-  });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
