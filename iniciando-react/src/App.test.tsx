ec19cc79e5ad2845fab8c2ab7c222f9255f6ab72
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-26 00:38:09 -0400

    ESlint TypeScript Config

--- iniciando-react/src/App.test.tsx
+++ iniciando-react/src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Test App', () => {
+  it('Deve renderizar sem erros', () => {
+    const { getByText } = render(<App />);
+
+    expect.assertions(1);
+    expect(getByText('Teste')).toBeTruthy();
+  });
 });
