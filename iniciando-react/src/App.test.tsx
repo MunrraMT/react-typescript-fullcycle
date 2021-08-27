64613f7dc289cd40720bc102eddd456ef579361a
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-27 07:32:59 -0400

    Add expect

--- iniciando-react/src/App.test.tsx
+++ iniciando-react/src/App.test.tsx
@@ -2,5 +2,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  const { getByText } = render(<App />);
+
+  expect(getByText('Teste')).toBeInTheDocument();
 });
