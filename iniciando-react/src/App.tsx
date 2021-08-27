8f24e1a39c9fb2e603c444fcc2adcaade58e0f07
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-27 13:32:54 -0400

    Form Style

--- iniciando-react/src/App.tsx
+++ iniciando-react/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import Form from './form';
+
+import Form from './Form';
 
 function App(): JSX.Element {
   return (
---
+++ iniciando-react/src/Form.tsx
@@ -0,0 +1,47 @@
+import { FormEvent, useRef } from 'react';
+
+import './Form.css';
+
+const Form = (): JSX.Element => {
+  const inputNameRef = useRef<HTMLInputElement>(null);
+  const inputEmailRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    console.log(
+      `Nome: ${inputNameRef.current?.value} / Email: ${inputEmailRef.current?.value}`,
+    );
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <label className="form-label" htmlFor="name">
+        Nome:
+        <input
+          className="form-input"
+          type="text"
+          id="name"
+          ref={inputNameRef}
+        />
+      </label>
+
+      <label className="form-label" htmlFor="email">
+        Email:
+        <input
+          className="form-input"
+          type="email"
+          name="email"
+          id="email"
+          ref={inputEmailRef}
+        />
+      </label>
+
+      <button className="form-btn" type="submit">
+        Enviar
+      </button>
+    </form>
+  );
+};
+
+export default Form;
